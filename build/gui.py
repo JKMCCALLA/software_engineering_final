e3378a5de39d6bbd267236321ede0889eda5c9a0
JKMCCALLA <59835986+JKMCCALLA@users.noreply.github.com>
2023-02-06 11:20:24 -0500

    Folder Changes

--- build/gui.py
+++ build/gui.py
@@ -7,7 +7,9 @@ from PIL import Image, ImageTk
 
 #===================================Assets Declaration
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/Software Engineering/build/assets/frame0")
+# ASSETS_PATH = OUTPUT_PATH / Path(r"Users/jordanemccalla/Downloads/Software Engineering/build/assets/frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"/Users/jordanemccalla/Downloads/Software Engineering/softeng_team2023/build/assets/frame0")
+
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
@@ -179,7 +181,7 @@ def HomeWindow():
     screen_height = window.winfo_screenheight()
     x = (screen_width/2) - (width/2)
     y = (screen_height/2) - (height/2)
-    window.resizable(0, 0)
+    window.resizable(1, 1)
     Home.geometry("%dx%d+%d+%d" % (width, height, x, y))
     lbl_home = Label(Home, text="Successfully Login!", font=('times new roman', 20)).pack()
     btn_back = Button(Home, text='Back', command=Back).pack(pady=20, fill=X)
